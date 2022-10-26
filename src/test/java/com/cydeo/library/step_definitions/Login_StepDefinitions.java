aeb57b23923178377b1b28cfa5162de84917ed6b
A Nuri <ahmettonuri@gmail.com>
2022-10-26 20:28:57 +0300

    Login_StepDefinitions.java is added

---
+++ src/test/java/com/cydeo/library/step_definitions/Login_StepDefinitions.java
@@ -0,0 +1,42 @@
+package com.cydeo.library.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_StepDefinitions {
+
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        System.out.println("User is on the login page");
+
+    }
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+        System.out.println("user enters librarian username");
+    }
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        System.out.println("user enters librarian password");
+    }
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        System.out.println("user should see the dashboard");
+    }
+
+
+
+
+    @When("user enters student username")
+    public void user_enters_student_username() {
+        System.out.println("user enters student username");
+    }
+    @When("user enters student password")
+    public void user_enters_student_password() {
+        System.out.println("user enters student password");
+    }
+
+
+
+}//endclass
