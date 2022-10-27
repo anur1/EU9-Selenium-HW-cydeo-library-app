117c6748a30e60112a3c9ba9affbbbc1769ebdf5
A Nuri <ahmettonuri@gmail.com>
2022-10-27 13:12:17 +0300

    plug-in added to CukesRunner.java

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -6,9 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "json:target/cucumber.json",
         features = "src/test/resources/features",
-        glue = "com/cydeo/library/step_definitions"
-
+        glue = "com/cydeo/library/step_definitions",
+        dryRun = false,
+        tags = "",
+        publish = false
 
 )
 public class CukesRunner {
