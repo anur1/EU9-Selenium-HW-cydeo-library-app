91d60d134d3a11de3dd94fef7df37b955e9b4062
A Nuri <ahmettonuri@gmail.com>
2022-10-27 13:41:33 +0300

    CukesRunner.java is revised

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/library/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@task5",
         publish = false
 
 )
